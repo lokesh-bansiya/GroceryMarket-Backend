76d59f7b09029e26956657441fa5a42bb94fcd8b
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-01-27 11:15:17 +0530

    Grocery Mart

--- middlewares/Auth.middleware.js
+++ middlewares/Auth.middleware.js
@@ -4,16 +4,16 @@ require('dotenv').config();
 const AuthValidator = (req, res, next) => {
     const token = req.headers.authorization;
     console.log(token);
-    if (token) {
+    // if (token) {
         const decoded = jwt.verify(token, process.env.key);
         if (decoded.userID) {
             next();
         } else {
             res.send({ Message: "Please Login First 1" });
         }
-    } else {
-        res.send({ Message: "Please Login First 2" });
-    }
+    // } else {
+    //     res.send({ Message: "Please Login First 2" });
+    // }
 };
 
 module.exports = { AuthValidator };
