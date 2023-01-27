a49d9d6c8f98ef3c4d8603c746595eeae44e6971
Lokesh_Patidar <bansiyalokesh29988@gmail.com>
2023-01-27 11:04:49 +0530

    gm

--- middlewares/Auth.middleware.js
+++ middlewares/Auth.middleware.js
@@ -3,17 +3,17 @@ require('dotenv').config();
 
 const AuthValidator = (req, res, next) => {
     const token = req.headers.authorization;
-    console.log(token);
-    if (token) {
+    // console.log(token);
+    // if (token) {
         const decoded = jwt.verify(token, process.env.key);
         if (decoded.userID) {
             next();
         } else {
             res.send({ Message: "Please Login First 1" });
         }
-    } else {
-        res.send({ Message: "Please Login First 2" });
-    }
+    // } else {
+    //     res.send({ Message: "Please Login First 2" });
+    // }
 };
 
 module.exports = { AuthValidator };
